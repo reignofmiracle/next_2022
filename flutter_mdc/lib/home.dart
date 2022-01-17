40f98338b32725cc829ff5254b4cf371b8763211
reign <reignofmiracle@gmail.com>
2022-01-17 18:41:33 +0900

    working on mdc 102

--- flutter_mdc/lib/home.dart
+++ flutter_mdc/lib/home.dart
@@ -3,12 +3,82 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+      (int index) => Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Icon(
+                Icons.favorite,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Title'),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  Text('Secondary Text')
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+    return cards;
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Scaffold(
-      body: Center(
-        child: Text('You did it'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SHRINE'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(10),
       ),
       resizeToAvoidBottomInset: false,
     );
